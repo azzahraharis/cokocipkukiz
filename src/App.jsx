93ef64bd683b9fb2685400aad00efa4f98932805
azzahraharis <73114918+azzahraharis@users.noreply.github.com>
2021-04-20 16:50:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,8 @@ export default function App() {
                     <div className="profile-wrapper" ref={profileWrapperRef}>
                         <img src={avatarSrc} alt="Avatar" className="avatar-img" />
                         
-                        <h1>Nama Kamu</h1>
-                        <p>Deskripsi apa pun di sini</p>
+                        <h1>Cokocipkukiz</h1>
+                        <p>Free form wanderer</p>
                     </div>
                 </section>
 
